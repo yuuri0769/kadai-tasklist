8151ca6d150aa9b76e3e6f4298c5afc3dc9ba75b
yuuri769 <yrri0769@gmail.com>
2021-08-03 02:08:32 +0900

    Add validation

--- src/controllers/CreateServlet.java
+++ src/controllers/CreateServlet.java
@@ -2,8 +2,10 @@ package controllers;
 
 import java.io.IOException;
 import java.sql.Timestamp;
+import java.util.List;
 
 import javax.persistence.EntityManager;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -12,6 +14,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import models.Task;
 import utils.DBUtil;
+import validators.TaskValidator;
 
 /**
  * Servlet implementation class CreateServlet
@@ -47,14 +50,29 @@ public class CreateServlet extends HttpServlet {
 		    t.setCreated_at(currentTime);
 		    t.setUpdated_at(currentTime);
 
+		    // バリデーションを実行してエラーがあったら新規登録フォームに戻る
+		    List<String> errors = TaskValidator.validate(t);
+		    if(errors.size() > 0) {
+		        em.close();
+
+		        // フォームに初期値を設定、さらにエラーメッセージを送る
+		        request.setAttribute("_token", request.getSession().getId());
+		        request.setAttribute("task", t);
+		        request.setAttribute("errors", errors);
+
+		        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/tasks/new.jsp");
+		        rd.forward(request, response);
+		    } else {
+		    // データベースに保存
 		    em.getTransaction().begin();
 		    em.persist(t);
 		    em.getTransaction().commit();
 		    request.getSession().setAttribute("flush", "登録が完了しました。");
 		    em.close();
 
+		    // indexのページリダイレクト
 		    response.sendRedirect(request.getContextPath() + "/index");
 		}
 	}
-
+	}
 }
--- src/controllers/UpdateServlet.java
+++ src/controllers/UpdateServlet.java
@@ -2,8 +2,10 @@ package controllers;
 
 import java.io.IOException;
 import java.sql.Timestamp;
+import java.util.List;
 
 import javax.persistence.EntityManager;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -12,6 +14,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import models.Task;
 import utils.DBUtil;
+import validators.TaskValidator;
 
 /**
  * Servlet implementation class UpdateServlet
@@ -49,6 +52,19 @@ public class UpdateServlet extends HttpServlet {
 	        Timestamp currentTime = new Timestamp(System.currentTimeMillis());
 	        t.setUpdated_at(currentTime);     // 更新日時のみ上書き
 
+	        // バリデーションを実行してエラーがあったら編集画面のフォームに戻る
+	        List<String> errors = TaskValidator.validate(t);
+	        if(errors.size() > 0) {
+	            em.close();
+
+	            //フォームに初期値を設定、さらにエラーメッセージを送る
+	            request.setAttribute("_token",request.getSession().getId());
+	            request.setAttribute("task", t);
+	            request.setAttribute("errors", errors);
+
+	            RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/tasks/edit.jsp");
+	            rd.forward(request, response);
+	        } else {
 	        // データベースを更新
 	        em.getTransaction().begin();
 	        em.getTransaction().commit();
@@ -60,7 +76,7 @@ public class UpdateServlet extends HttpServlet {
 
 	        // indexページへリダイレクト
 	        response.sendRedirect(request.getContextPath() + "/index");
+	        }
 	    }
 	}
-
 }
